0677f68c00635d2034dd7caed9784f6f54c58df4
Aryaman22102002 <aryashardul2002@gmail.com>
2023-07-14 21:18:12 +0530

    Final Grid code is updated

--- Generate_Final_Grid.py
+++ Generate_Final_Grid.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageOps
 import numpy as np 
 import matplotlib.pyplot as plt
 
-img = Image.open("obstacles.png")
+img = Image.open("Images/Obstacles.png")
 img = ImageOps.grayscale(img)
 
 np_img = np.array(img)
@@ -11,9 +11,9 @@ np_img[np_img > 0] = 1
 plt.set_cmap('binary')
 plt.imshow(np_img)
 
-np.save("grid.npy", np_img)
+np.save("Images/Grid.npy", np_img)
 
-grid = np.load("grid.npy")
-plt.imshow(grid, cmap = 'binary')
-plt.tight_layout()
-plt.show()
+#grid = np.load("Images/Grid.npy")
+#plt.imshow(grid, cmap = 'binary')
+#plt.tight_layout()
+#plt.show()
