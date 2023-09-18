01f9e56139de526bbb80563a48d4c796bf2c7d92
Aryaman22102002 <aryashardul2002@gmail.com>
2023-09-19 01:51:28 +0530

    Paramater changed

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('python3 Generate_Obstacles.py input')
+os.system('python3 Generate_Obstacles.py')
 os.system('python3 Squaring_Figure.py')
 os.system('python3 Generate_Final_Grid.py')
 os.system('python3 RRT.py')
